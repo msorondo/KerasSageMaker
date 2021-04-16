c3b57dde147d9b536032d3427c109b41e7665031
Martin <msorondo>
2021-04-16 22:35:38 +0000

    agrego faltantes

---
+++ train.py
@@ -0,0 +1,106 @@
+import argparse, os
+import boto3
+import numpy as np
+import pandas as pd
+import sagemaker
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
+
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--epochs', type=int, default=2)
+    parser.add_argument('--learning-rate', type=float, default=0.001)
+    
+    parser.add_argument('--sm-model-dir', type=str, default=os.environ.get('SM_MODEL_DIR'))
+    parser.add_argument('--model_dir', type=str)
+    parser.add_argument('--train', type=str, default=os.environ.get('SM_CHANNEL_TRAIN'))
+    
+    args, _ = parser.parse_known_args()
+    epochs     = args.epochs
+    lr         = args.learning_rate
+    model_dir  = args.model_dir
+    sm_model_dir = args.sm_model_dir
+    training_dir   = args.train
+    
+    
+    train_dir = os.path.join(training_dir, "train")
+    test_dir = os.path.join(training_dir, "validation")
+    print(train_dir, test_dir)
+
+    IMG_SIZE = (256, 256)
+    BATCH_SIZE = 32
+
+    train_val_dataset = image_dataset_from_directory(train_dir,
+                                                 shuffle=True,
+                                                 batch_size=BATCH_SIZE,
+                                                 image_size=IMG_SIZE)
+
+    test_dataset = image_dataset_from_directory(test_dir,
+                                                shuffle=True,
+                                                batch_size=BATCH_SIZE,
+                                                image_size=IMG_SIZE)
+
+    train_val_batches = tf.data.experimental.cardinality(train_val_dataset)
+    validation_dataset = train_val_dataset.take(train_val_batches // 10)
+    train_dataset = train_val_dataset.skip(train_val_batches // 10)
+    
+    
+    data_augmentation = tf.keras.Sequential([
+      tf.keras.layers.experimental.preprocessing.RandomFlip('horizontal'),
+      tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
+    ])
+    preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
+    IMG_SHAPE = IMG_SIZE + (3,)
+    base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                               include_top=False,
+                                               weights='imagenet')
+    base_model.trainable = True
+
+    # Fine-tune from this layer onwards
+    fine_tune_at = 100
+
+    # Freeze all the layers before the `fine_tune_at` layer
+    for layer in base_model.layers[:fine_tune_at]:
+        layer.trainable =  False
+    
+    global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+    prediction_layer = tf.keras.layers.Dense(1)
+    
+    inputs = tf.keras.Input(shape=(256, 256, 3))
+    x = data_augmentation(inputs)
+    x = preprocess_input(x)
+    x = base_model(x)
+    x = global_average_layer(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+    outputs = prediction_layer(x)
+    model = tf.keras.Model(inputs, outputs)
+    
+    base_learning_rate = 0.0001
+    model.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+    
+    initial_epochs = 8
+
+    history = model.fit(train_dataset,
+                    epochs=initial_epochs,
+                    validation_data=validation_dataset)
+    
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    
+    
+    model.save(os.path.join(sm_model_dir, '000000001'), 'my_model.h5')
