8fd3840cb09b5ea52e2b3fd21b16fa34c6f28874
Martin <msorondo>
2021-04-16 22:34:32 +0000

    update

--- .ipynb_checkpoints/cats_n_dogs_model-checkpoint.py
+++ .ipynb_checkpoints/cats_n_dogs_model-checkpoint.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.python.keras import Sequential
-from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, BatchNormalization
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.python.training.rmsprop import RMSPropOptimizer
 
@@ -8,38 +8,39 @@ HEIGHT = 128
 WIDTH = 128
 DEPTH = 3
 NUM_CLASSES = 2
-BATCH_SIZE = 64
+BATCH_SIZE = 15
 INPUT_TENSOR_NAME = "inputs_input" 
+FAST_RUN = False
+IMAGE_WIDTH=128
+IMAGE_HEIGHT=128
+IMAGE_SIZE=(IMAGE_WIDTH, IMAGE_HEIGHT)
+IMAGE_CHANNELS=3
 
 def keras_model_fn(hyperparameters):
     model = Sequential()
-
-    model.add(Conv2D(32, kernel_size=(3, 3), input_shape=(HEIGHT, WIDTH, DEPTH), activation="relu", name="inputs",
-                     padding="same"))
-    model.add(MaxPooling2D())
-    model.add(Conv2D(32, kernel_size=(3, 3), activation="relu"))
-    model.add(MaxPooling2D())
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
-    model.add(Conv2D(64, kernel_size=(3, 3), activation="relu", padding="same"))
-    model.add(MaxPooling2D())
-    model.add(Conv2D(64, kernel_size=(3, 3), activation="relu"))
-    model.add(MaxPooling2D())
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
-    model.add(Conv2D(128, kernel_size=(3, 3), activation="relu", padding="same"))
-    model.add(MaxPooling2D())
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
+
     model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+    model.add(Dense(2, activation='softmax')) # 2 because we have cat and dog classes
 
-    model.add(Dense(256, activation="relu"))
-    model.add(Dense(256, activation="relu"))
-    model.add(Dropout(0.25))
-    model.add(Dense(2, activation="softmax"))
 
-    opt = RMSPropOptimizer(learning_rate=hyperparameters['learning_rate'], decay=hyperparameters['decay'])
-
-    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=["accuracy"])
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
     return model
 
 
@@ -73,3 +74,16 @@ def _input(mode, batch_size, data_dir):
     images, labels = generator.next()
 
     return {INPUT_TENSOR_NAME: images}, labels
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+
+    # Data, model, and output directories
+    # model_dir is always passed in from SageMaker. By default this is a S3 path under the default bucket.
+    parser.add_argument('--model_dir', type=str)
+    parser.add_argument('--sm-model-dir', type=str, default=os.environ.get('SM_MODEL_DIR'))
+    parser.add_argument('--train', type=str, default=os.environ.get('SM_CHANNEL_TRAINING'))
+    parser.add_argument('--hosts', type=list, default=json.loads(os.environ.get('SM_HOSTS')))
+    parser.add_argument('--current-host', type=str, default=os.environ.get('SM_CURRENT_HOST'))
+
+    return parser.parse_known_args()
